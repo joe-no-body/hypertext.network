a45bccc5f448f7c6297cd7f5cd91f09f04840d62
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2020-05-22 20:28:06 -0700

    allow rewriting relative urls

--- rewrite-urls.lua
+++ rewrite-urls.lua
@@ -1,6 +1,6 @@
 # via https://stackoverflow.com/a/49396058
 function Link(el)
-    if el.target:find("/") == 1 then
+    if el.target:find("/") == 1 or el.target:find(".") == 1 then
         el.target = string.gsub(el.target, "%.md", ".html")
     end
     return el
